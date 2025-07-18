953aa733ae00618f8d23ad3844a554c6f5d084c9
FiretorchTM <pyettropsi@gmail.com>
2025-07-17 22:02:56 -0300

    aula-03:  Verdadeiro e falso

---
+++ aula-03.js
@@ -0,0 +1,17 @@
+let idade1 = 18;
+let idade2 = 22;
+const nome = "VIP";
+const idade_min = 18;
+
+if(idade1 && idade2 < idade_min || nome !=="VIP")
+{
+    console.log("Entrada Negada!");
+}
+
+    else
+{
+    console.log("Entrada Permitida!");
+}
+
+
+
