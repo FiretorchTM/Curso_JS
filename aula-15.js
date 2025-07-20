6dc979124076b59250ea458da613d9dae74b4529
FiretorchTM <pyettropsi@gmail.com>
2025-07-20 19:04:45 -0300

    aula-15: variaveis primitivas imutaveis

---
+++ aula-15.js
@@ -0,0 +1,10 @@
+const numero = 4321;//primitivo e imutavel
+const mensagem = "nome";//primitivo e imutavel
+const verdadeiroFalse = true;//false primitivo e imutavel
+
+
+const arrayTest = [2,4,6];
+
+arrayTest.push(10);
+
+console.log(arrayTest);
